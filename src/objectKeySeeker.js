c193be7865f02e8f826e159168927b2cac4adc25
derban7ikus <derbantikus@yandex.ru>
2022-03-02 16:46:38 +0300

    github actions test fix

--- src/objectKeySeeker.js
+++ src/objectKeySeeker.js
@@ -1,19 +1,21 @@
+import _ from "lodash";
+
 const isInFirstObjectOnly = (object1, object2, key) => {
-  if (Object.hasOwn(object1, key) && !Object.hasOwn(object2, key)) {
+  if (_.has(object1, key) && !_.has(object2, key)) {
     return true;
   }
   return false;
 };
 
 const isInSecondObjectOnly = (object1, object2, key) => {
-  if (!Object.hasOwn(object1, key) && Object.hasOwn(object2, key)) {
+  if (!_.has(object1, key) && _.has(object2, key)) {
     return true;
   }
   return false;
 };
 
 const isInBothObjects = (object1, object2, key) => {
-  if (Object.hasOwn(object1, key) && Object.hasOwn(object2, key)) {
+  if (_.has(object1, key) && _.has(object2, key)) {
     return true;
   }
   return false;
