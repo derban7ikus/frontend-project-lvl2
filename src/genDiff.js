9cdaff9ec4179aa05d3aaa02eebe5b90d9a88383
derban7ikus <derbantikus@yandex.ru>
2022-03-02 16:39:49 +0300

    linter fixes

--- src/genDiff.js
+++ src/genDiff.js
@@ -10,26 +10,23 @@ const genDiff = (filepath1, filepath2) => {
   const mergedKeys = Object.keys(mergedObject);
   const sortedKeys = _.sortBy(mergedKeys);
 
-  const diffSeeker = (keys) => {
-    const result = [];
-    for (const key of keys) {
-      if (isInFirstObjectOnly(obj1, obj2, key)) {
-        result.push([ `- ${key}: ${obj1[ key ]}` ]);
-      } else if (isInSecondObjectOnly(obj1, obj2, key)) {
-        result.push([ `+ ${key}: ${obj2[ key ]}` ]);
-      } else if (isInBothObjects(obj1, obj2, key) && obj1[ key ] === obj2[ key ]) {
-        result.push([ `  ${key}: ${obj1[ key ]}` ]);
-      } else {
-        result.push([ `- ${key}: ${obj1[ key ]}` ]);
-        result.push([ `+ ${key}: ${obj2[ key ]}` ]);
-      }
+  const temp = (result, key) => {
+    if (isInFirstObjectOnly(obj1, obj2, key)) {
+      result.push([ `- ${key}: ${obj1[ key ]}` ]);
+    } else if (isInSecondObjectOnly(obj1, obj2, key)) {
+      result.push([ `+ ${key}: ${obj2[ key ]}` ]);
+    } else if (isInBothObjects(obj1, obj2, key) && obj1[ key ] === obj2[ key ]) {
+      result.push([ `  ${key}: ${obj1[ key ]}` ]);
+    } else {
+      result.push([ `- ${key}: ${obj1[ key ]}` ]);
+      result.push([ `+ ${key}: ${obj2[ key ]}` ]);
     }
-    return result.join(`
-    `);
+    return result;
   };
 
   return `{
-    ${diffSeeker(sortedKeys)}
+    ${sortedKeys.reduce(temp, []).join(`
+    `)}
 }`;
 };
 
--- src/parsers.js
+++ src/parsers.js
@@ -3,15 +3,13 @@ import path from 'path';
 import yaml from 'js-yaml';
 
 const parser = (filepath) => {
-  const absolutePath = (filepath) => path.resolve(process.cwd(), filepath);
+  const absolutePath = (pathToFile) => path.resolve(process.cwd(), pathToFile);
   const data = readFileSync(absolutePath(filepath), 'utf8');
   const format = path.extname(filepath);
 
   if (format === '.yml' || format === '.yaml') {
     return yaml.load(data);
-  } else if (format === '.json') {
-    return JSON.parse(data);
-  }
+  } return JSON.parse(data);
 };
 
 export default parser;
