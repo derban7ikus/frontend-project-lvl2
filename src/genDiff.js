08424059464ee43491087f90a8284ff8333eb282
derban7ikus <derbantikus@yandex.ru>
2022-03-11 22:00:09 +0300

    next step 1/2

--- src/genDiff.js
+++ src/genDiff.js
@@ -6,28 +6,29 @@ const genDiff = (filepath1, filepath2) => {
   const obj1 = parser(filepath1);
   const obj2 = parser(filepath2);
 
-  const mergedObject = _.merge({}, obj1, obj2);
-  const mergedKeys = Object.keys(mergedObject);
-  const sortedKeys = _.sortBy(mergedKeys);
+  const differ = (object1, object2) => {
+    const mergedObject = _.merge({}, object1, object2);
+    const mergedKeys = Object.keys(mergedObject);
+    const sortedKeys = _.sortBy(mergedKeys);
 
-  const temp = (result, key) => {
-    if (isInFirstObjectOnly(obj1, obj2, key)) {
-      result.push([ `- ${key}: ${obj1[ key ]}` ]);
-    } else if (isInSecondObjectOnly(obj1, obj2, key)) {
-      result.push([ `+ ${key}: ${obj2[ key ]}` ]);
-    } else if (isInBothObjects(obj1, obj2, key) && obj1[ key ] === obj2[ key ]) {
-      result.push([ `  ${key}: ${obj1[ key ]}` ]);
-    } else {
-      result.push([ `- ${key}: ${obj1[ key ]}` ]);
-      result.push([ `+ ${key}: ${obj2[ key ]}` ]);
+    const temp = (result, key) => {
+      if (_.isObject(object1[ key ]) && _.isObject(object2[ key ])) {
+        result[ ` ${key}` ] = differ(object1[ key ], object2[ key ]);
+      } else if (isInFirstObjectOnly(object1, object2, key)) {
+        result[ `- ${key}` ] = object1[ key ];
+      } else if (isInSecondObjectOnly(object1, object2, key)) {
+        result[ `+ ${key}` ] = object2[ key ];
+      } else if (isInBothObjects(object1, object2, key) && object1[ key ] === object2[ key ]) {
+        result[ ` ${key}` ] = object1[ key ];
+      } else {
+        result[ `- ${key}` ] = object1[ key ];
+        result[ `+ ${key}` ] = object2[ key ];
+      }
+      return result;
     }
-    return result;
-  };
-
-  return `{
-    ${sortedKeys.reduce(temp, []).join(`
-    `)}
-}`;
+    return sortedKeys.reduce(temp, {});
+  }
+  return JSON.stringify(differ(obj1, obj2));
 };
 
 export default genDiff;
