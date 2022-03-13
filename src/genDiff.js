d0423c1c5e2d4b1d2832ff9d52b3e721477143d4
derban7ikus <derbantikus@yandex.ru>
2022-03-13 13:17:21 +0300

    6th step

--- src/genDiff.js
+++ src/genDiff.js
@@ -1,6 +1,7 @@
 import _ from 'lodash';
 import { isInFirstObjectOnly, isInSecondObjectOnly, isInBothObjects } from './objectKeySeeker.js';
 import parser from './parsers.js';
+import stringify from './stylish.js';
 
 const genDiff = (filepath1, filepath2) => {
   const obj1 = parser(filepath1);
@@ -13,13 +14,13 @@ const genDiff = (filepath1, filepath2) => {
 
     const temp = (result, key) => {
       if (_.isObject(object1[ key ]) && _.isObject(object2[ key ])) {
-        result[ ` ${key}` ] = differ(object1[ key ], object2[ key ]);
+        result[ `  ${key}` ] = differ(object1[ key ], object2[ key ]);
       } else if (isInFirstObjectOnly(object1, object2, key)) {
         result[ `- ${key}` ] = object1[ key ];
       } else if (isInSecondObjectOnly(object1, object2, key)) {
         result[ `+ ${key}` ] = object2[ key ];
       } else if (isInBothObjects(object1, object2, key) && object1[ key ] === object2[ key ]) {
-        result[ ` ${key}` ] = object1[ key ];
+        result[ `  ${key}` ] = object1[ key ];
       } else {
         result[ `- ${key}` ] = object1[ key ];
         result[ `+ ${key}` ] = object2[ key ];
@@ -28,7 +29,7 @@ const genDiff = (filepath1, filepath2) => {
     }
     return sortedKeys.reduce(temp, {});
   }
-  return JSON.stringify(differ(obj1, obj2));
+  return stringify(differ(obj1, obj2));
 };
 
 export default genDiff;
