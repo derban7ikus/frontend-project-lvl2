575fbedf130f6796d6fbcd9e7b7e67c2bd88a2d7
derban7ikus <92376241+derban7ikus@users.noreply.github.com>
2022-03-25 15:59:13 +0300

    Update differ.js

--- src/differ.js
+++ src/differ.js
@@ -7,18 +7,23 @@ const differ = (object1, object2) => {
   const sortedKeys = _.sortBy(mergedKeys);
 
   const temp = sortedKeys.map((key) => {
-    const value1 = object1[ key ];
-    const value2 = object2[ key ];
+    const value1 = object1[key];
+    const value2 = object2[key];
     if (_.isObject(value1) && _.isObject(value2)) {
       return { type: 'recursion', keey: key, value: differ(value1, value2) };
     } if (isInFirstObjectOnly(object1, object2, key)) {
       return { type: 'firstObject', keey: key, value: value1 };
     } if (isInSecondObjectOnly(object1, object2, key)) {
       return { type: 'secondObject', keey: key, value: value2 };
-    } if (isInBothObjects(object1, object2, key) && object1[ key ] === object2[ key ]) {
+    } if (isInBothObjects(object1, object2, key) && object1[key] === object2[key]) {
       return { type: 'bothEqual', keey: key, value: value1 };
     }
-    return { type: 'bothNonEqual', keey: key, val1: value1, val2: value2 };
+    return {
+      type: 'bothNonEqual', 
+      keey: key, 
+      val1: value1, 
+      val2: value2 
+    };
 
   });
   return temp;
